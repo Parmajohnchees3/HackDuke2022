5dd311a76d68550f942d239c1399b289138884f7
Aryan Mathur <34112101+Aryan27Mathur@users.noreply.github.com>
2022-10-22 19:37:45 +0000

    reorganized files, added responsive front page

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
+import app.app  
 
 
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.route('/features')
-def features():
-    return "FEATURES"
-
-@app.route('/pricing')
-def pricing():
-    return "PRICING"
-
-app.run(host='0.0.0.0', port=81)
